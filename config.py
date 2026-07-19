b57ade74bacbf2124b1c6dfb1fcfe5f4bea6a313
hema951code <22f1000951@ds.study.iitm.ac.in>
2026-07-19 21:16:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # ============================================================
 
 EMAIL = "22f1000951@ds.study.iitm.ac.in"
-AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIyZjEwMDA5NTFAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4Mzc0MzYwNywiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NDM0ODQwN30.A4X16V72uLLoRBmiUXpVSuWefo6Ad7sU279NSKBlD8c"
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIyZjEwMDA5NTFAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDQ3NTY2MCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTA4MDQ2MH0.KXMZ-qIdOS9lEFURzL3b8dYRCXlDV65aevTZdHnaHGc"
 
 # Fixed — do not change
 AIPIPE_BASE = "https://aipipe.org/openai/v1"
